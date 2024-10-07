d8443e38740f424a432cabc8dccdfdbe8bd710cf
Algoz49 <robertwilianmesquita@gmail.com>
2024-10-07 11:50:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
-int main()
-{
-    printf("oi");
-    return 0;
+int main(){
+    int altura;
+    int peso;
+    int imc;
+    printf("Digite seu peso");
+    scanf("&d", &peso);
+    printf("qual sua altura em cm");
+    scanf("%d", &altura);
+    imc = peso / (altura * altura);
+    printf ("%d", imc);
+
 }
